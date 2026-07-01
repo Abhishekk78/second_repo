caafa482e40c6451b32c1ab212a1e500488a6a33
abhishekpandya410@gmail.com <abhishkpandya410@gmail.com>
2026-07-01 12:11:25 +0530

    Filament(Project)

---
+++ Filamentt(Project)/Frequencycounter.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+    char str[100];
+    int i,j,len,count;
+    
+    printf("Enter any string :");
+    scanf("%[^\n]",str);
+    
+    len = strlen(str);
+    
+    for(i=0;i<len;i++){
+    
+      count =1;
+        
+        if(str[i] == '\0'){
+            continue;
+        }
+        
+        for(j=i+1;j<len;j++){
+            
+            if(str[i] == str[j]){
+                count++;
+                str[j] = '\0';
+            }
+        }
+    printf("%c=>%d\n",str[i],count);
+    }
+        return 0;
+    
+}
---
+++ Filamentt(Project)/Palindrome.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+bool checkpalindrome(char str[]){
+
+    int l = strlen(str);
+    int right = l-1;
+    int left = 0;
+
+    while(left < right){
+
+        if(str[left] != str[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main(){
+
+    char str[] = "madamdadas";
+
+    if(checkpalindrome(str)){
+        printf("It is palindrome ");
+    }
+    else{
+        printf("It is not palindrome ");
+    }
+    return 0;
+}
